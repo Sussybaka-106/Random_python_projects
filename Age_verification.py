e9dc9bb0a8c3f47f07e1b37b8891d0d2bbe15e1d
Sussybaka-106 <sharmanavnit43@gmail.com>
2026-09-16 21:38:17 +0530

    add the Age_verificatioin project

---
+++ Age_verification.py
@@ -0,0 +1,15 @@
+age = int(input("Enter youre age usee: "))
+
+if age < 10:
+    print("Your still a child")
+
+elif age < 20:
+    print("Your a young adult")
+
+elif age < 45:
+    print("Your a adult")
+
+elif age < 60:
+    print("Your a gradpa")
+
+
